7001dd55134e5bf9309b2e4a497466011ae7bdab
happynao <244932644@qq.com>
2019-10-07 17:18:25 +0800

    add html reports

---
+++ tntfuzzer/utils/generate_html.py
@@ -0,0 +1,19 @@
+from jinja2 import Environment, FileSystemLoader
+
+def generate_html(body):
+    env = Environment(loader=FileSystemLoader('./'))
+    template = env.get_template('utils/template.html')
+    with open("result.html", 'w+') as fout:
+        html_content = template.render(body=body)
+        fout.write(html_content)
+
+
+if __name__ == "__main__":
+    body = []
+    obj = {"op_code": "GET", "url": "abc", "status_code": 500,
+           "response_msg": "ok", "body": "body", "curl_command": "curl_command"}
+    obj2 = {"op_code": "POST", "url": "abc", "status_code": 500,
+           "response_msg": "ok", "body": "body", "curl_command": "curl_command"}
+    body.append(obj)
+    body.append(obj2)
+    generate_html(body)
