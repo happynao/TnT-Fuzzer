51141006cf685fd6b38c60fc3225b0aba660be67
happynao <244932644@qq.com>
2019-10-09 09:46:27 +0800

    modify code

--- tntfuzzer/utils/generate_html.py
+++ tntfuzzer/utils/generate_html.py
@@ -7,23 +7,24 @@ from jinja2 import Environment, FileSystemLoader
 
 def generate_html(body, report_dir, build_id):
     env = Environment(loader=FileSystemLoader('./'))
-    template = env.get_template('tntfuzzer/utils/template.html')
+    template = env.get_template('utils/template.html')
     path = "{}/{}/".format(report_dir, build_id)
-    if not os.path.exists(os.path.dirname(path)):
+    if not os.path.exists(path):
         try:
-            os.makedirs(os.path.dirname(path))
+            os.makedirs(path)
         except OSError:
             pass
+
     with open(r"{}fuzz_testing_report.html".format(path), mode='w+', encoding='utf-8', buffering=1) as f:
         html_content = template.render(body=body)
         f.write(html_content)
 
 def generate_report(report_dir, files):
     env = Environment(loader=FileSystemLoader('./'))
-    template = env.get_template('tntfuzzer/utils/index.html')
-    if not os.path.exists(os.path.dirname(report_dir)):
+    template = env.get_template('utils/index.html')
+    if not os.path.exists(report_dir):
         try:
-            os.makedirs(os.path.dirname(report_dir))
+            os.makedirs(report_dir)
         except OSError:
             pass
     with open(r"{}index.html".format(report_dir), mode='w+', encoding='utf-8', buffering=1) as f:
@@ -41,9 +42,6 @@ def getListOfFiles(dirName):
         fullPath = os.path.join(dirName, entry)
         if os.path.isdir(fullPath):
             allFiles[entry] = os.path.join(fullPath, "fuzz_testing_report.html")
-        #     allFiles = allFiles + getListOfFiles(fullPath)
-        # else:
-        #     allFiles.append(fullPath)
     return allFiles
 
 # def path_leaf(path):
@@ -52,13 +50,13 @@ def getListOfFiles(dirName):
 #     return path1
 
 if __name__ == "__main__":
-    # body = []
-    # obj = {"op_code": "GET", "url": "abc", "status_code": 500,
-    #        "response_msg": "ok", "body": "body", "curl_command": "curl_command"}
-    # obj2 = {"op_code": "POST", "url": "abc", "status_code": 500,
-    #        "response_msg": "ok", "body": "body", "curl_command": "curl_command"}
-    # body.append(obj)
-    # body.append(obj2)
-    # generate_html(body, "/tmp/reports/")
-    list_of_files = getListOfFiles("/tmp/reports_test/")
+    body = []
+    obj = {"op_code": "GET", "url": "abc", "status_code": 500,
+           "response_msg": "ok", "body": "body", "curl_command": "curl_command"}
+    obj2 = {"op_code": "POST", "url": "abc", "status_code": 500,
+           "response_msg": "ok", "body": "body", "curl_command": "curl_command"}
+    body.append(obj)
+    body.append(obj2)
+    generate_html(body, "reports/", "1")
+    #list_of_files = getListOfFiles("/tmp/reports_test/")
     #generate_report("/tmp/reports/", list_of_files)
